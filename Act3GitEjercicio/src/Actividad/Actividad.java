c3d07acfaf0d6736fa5f26c6725204a4d257d773
Agustin Bernal <abersit270@g.educaand.es>
2024-11-26 09:53:45 +0100

    Modifiqué rama1

--- Act3GitEjercicio/src/Actividad/Actividad.java
+++ Act3GitEjercicio/src/Actividad/Actividad.java
@@ -1,9 +1,19 @@
 package Actividad;
 import java.util.Random;
+import java.util.Scanner;
 public class Actividad {
+	Random aleatorio= new Random();
+	Scanner lector=new Scanner(System.in);
+	public void entre2Numeros() {
+		int numero1=lector.nextInt();
+		int numero2=lector.nextInt();
+		for(int i=numero1;i<numero2; i+=7) {
+			
+		}
+	}
 	public int[] numerosAleatorios(){
 		int numeros[]=new int[20];
-		Random aleatorio= new Random();
+		
 		for(int i=0; i<numeros.length; i++) {
 			numeros[i]=aleatorio.nextInt(20);
 			
