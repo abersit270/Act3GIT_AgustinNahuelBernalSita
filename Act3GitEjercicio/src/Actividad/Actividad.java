34f0e5e89d6e7fca89186cb3cbd1685196c2ec65
Agustin Bernal <abersit270@g.educaand.es>
2024-11-26 09:35:11 +0100

    Añado el ignore y el readme

--- Act3GitEjercicio/src/Actividad/Actividad.java
+++ Act3GitEjercicio/src/Actividad/Actividad.java
@@ -1,5 +1,13 @@
 package Actividad;
-
+import java.util.Random;
 public class Actividad {
-
+	public int[] numerosAleatorios(){
+		int numeros[]=new int[20];
+		Random aleatorio= new Random();
+		for(int i=0; i<numeros.length; i++) {
+			numeros[i]=aleatorio.nextInt(20);
+			
+		}
+		return numeros;
+	}
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Esta es la actividad 3 de GIT realizado este Readme desde Elipse
+No voy a realizar esta parte tan extensa para concentrarme mejor en los códigos necesarios que hagan falta graciaas!!!
